fb683de886c2b3da1d594d0e2e67f37e171b5fad
SergioLV <sergio.lagos@mail.udp.cl>
2023-12-03 22:12:51 -0300

    changes

--- src/controllers/youtubeControllers.js
+++ src/controllers/youtubeControllers.js
@@ -1,44 +1,40 @@
 // controllers/youtubeController.js
-const youtube = require('../models/youtube');
-
+const youtube = require("../models/youtube");
 
 const youtubeController = {
-    getPlaylistVideos: async (req, res) => {
-<<<<<<< HEAD
-        console.log("youtube controller")
-=======
-        console.log("hola")
->>>>>>> 35e1d7be44b3297af0fa42d6b6f6224054b95ddf
-        const ID_playlist = req.params.id
-        console.log(typeof ID_playlist)
-        console.log("ID PLAY EN CONTROLLER", ID_playlist)
-        try {
-            youtube.playlistItems.list({
-                part: 'snippet',
-                maxResults: 50, // Puedes ajustar el número de resultados si es necesario
-                playlistId: ID_playlist,
-<<<<<<< HEAD
-=======
-                // playlistId: ID_playlist,
->>>>>>> 35e1d7be44b3297af0fa42d6b6f6224054b95ddf
-            }, (err, response) => {
-                if (err) {
-                    console.error('Error al obtener la lista de reproducción:', err);
-                    return res.status(500).json({ error: 'Error al obtener la lista de reproducción' });
-                }
+  getPlaylistVideos: async (req, res) => {
+    console.log("youtube controller");
+    const ID_playlist = req.params.id;
+    console.log(typeof ID_playlist);
+    console.log("ID PLAY EN CONTROLLER", ID_playlist);
+    try {
+      youtube.playlistItems.list(
+        {
+          part: "snippet",
+          maxResults: 50, // Puedes ajustar el número de resultados si es necesario
+          playlistId: ID_playlist,
+        },
+        (err, response) => {
+          if (err) {
+            console.error("Error al obtener la lista de reproducción:", err);
+            return res
+              .status(500)
+              .json({ error: "Error al obtener la lista de reproducción" });
+          }
 
-                const videos = response.data.items;
-                const videoInfo = videos.map(video => ({
-                    title: video.snippet.title,
-                    videoId: video.snippet.resourceId.videoId,
-                }));
+          const videos = response.data.items;
+          const videoInfo = videos.map((video) => ({
+            title: video.snippet.title,
+            videoId: video.snippet.resourceId.videoId,
+          }));
 
-                res.status(200).json({ videos: videoInfo });
-            });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Error al encontrar lista.' });
+          res.status(200).json({ videos: videoInfo });
         }
-    },
+      );
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error al encontrar lista." });
+    }
+  },
 };
 module.exports = youtubeController;
