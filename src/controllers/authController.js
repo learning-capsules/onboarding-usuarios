6b43cb354a302c0fd1d443caef4aa3aa0fcdd4da
Vicente Berroeta <vicente.berroeta@falp.org>
2023-12-04 04:59:30 -0300

    xcd

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,71 +22,41 @@ const authController = {
 
             // Si el correo no existe, continuar con el registro del usuario
             const hashedPassword = await bcrypt.hash(password, 10);
-            const response = await userModel.createUser(nombre, apellido, edad, correo, hashedPassword, id_tipo_aprendizaje);
+            const response = await userModel.createUser(
+                nombre,
+                apellido,
+                edad,
+                correo,
+                hashedPassword,
+                id_tipo_aprendizaje
+            );
             console.log(response);
-            res.status(200).json({ mensaje: 'Usuario registrado correctamente' });
+            res.status(200).json({ mensaje: "Usuario registrado correctamente" });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ error: 'Error al registrar el usuario.' });
+            res.status(500).json({ error: "Error al registrar el usuario." });
         }
     },
     loginUser: async (req, res) => {
         try {
-            let { correo, password } = req.body
-            const user = await userModel.getUser(correo)
+            let { correo, password } = req.body;
+            const user = await userModel.getUser(correo);
             if (!user) {
-                return res.status(401).json({ error: 'Credenciales inválidas.' });
+                return res.status(401).json({ error: "Credenciales inválidas." });
             }
             const isPasswordValid = await bcrypt.compare(password, user.password);
             if (!isPasswordValid) {
-                return res.status(401).json({ error: 'Credenciales inválidas.' });
+                return res.status(401).json({ error: "Credenciales inválidas." });
             }
-            console.log("usuario", user)
+            console.log("usuario", user);
             const token = generateAccessToken(user);
             const learningTypeId = user.id_tipo_aprendizaje;
-            res.json({ token, learningTypeId });
+            res.json({ token, learningTypeId, user });
         } catch (error) {
-            res.status(401).send('Credenciales inválidas.');
+            res.status(401).send("Credenciales inválidas.");
         }
     },
     // Otros métodos de controlador
-    // Si el correo no existe, continuar con el registro del usuario
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const response = await userModel.createUser(
-        nombre,
-        apellido,
-        edad,
-        correo,
-        hashedPassword,
-        id_tipo_aprendizaje
-    );
-    console.log(response);
-    res.status(200).json({ mensaje: "Usuario registrado correctamente" });
-} catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error al registrar el usuario." });
-}
-  },
-loginUser: async (req, res) => {
-    try {
-        let { correo, password } = req.body;
-        const user = await userModel.getUser(correo);
-        if (!user) {
-            return res.status(401).json({ error: "Credenciales inválidas." });
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: "Credenciales inválidas." });
-        }
-        console.log("usuario", user);
-        const token = generateAccessToken(user);
-        const learningTypeId = user.id_tipo_aprendizaje;
-        res.json({ token, learningTypeId, user });
-    } catch (error) {
-        res.status(401).send("Credenciales inválidas.");
-    }
-},
-  // Otros métodos de controlador
 };
 
 module.exports = authController;
